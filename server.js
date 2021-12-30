3454e708b374cfcc32096d1bc72e400cd10f308d
anthony solano lopez <anthonysn16@icloud.com>
2021-12-29 23:00:30 -0600

    se modificaron los nombres del proyecto para que coincidiera con el nombre del archivo de proyecto y funcionara el server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/control-de-registros'));
+app.use(express.static(__dirname + '/dist/frontend-control-de-registros'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/control-de-registros/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/frontend-control-de-registros/index.html'));
 });
 
 // default Heroku port
